ce8e8fb7f6a49a54bdb332309b8baa4e9807ecac
Renan Reis Martins de Paula <renan.reis@klarna.com>
2018-10-03 23:31:13 +0200

    copied prepend from the book but made the reverse :|

--- fpinjava-parent/fpinjava-makingjavafunctional-exercises/src/main/java/com/fpinjava/makingjavafunctional/exercise03_09/CollectionUtilities.java
+++ fpinjava-parent/fpinjava-makingjavafunctional-exercises/src/main/java/com/fpinjava/makingjavafunctional/exercise03_09/CollectionUtilities.java
@@ -70,10 +70,10 @@ public class CollectionUtilities {
   }
 
   public static <T> List<T> prepend(T t, List<T> list) {
-    throw new RuntimeException("To be implemented");
+    return foldLeft(list, list(t), a -> b -> append(a, b)); // copied from book
   }
 
   public static <T> List<T> reverse(List<T> list) {
-    throw new RuntimeException("To be implemented");
+    return foldLeft(list, Arrays.asList(), a -> b -> prepend(b, a));
   }
 }
