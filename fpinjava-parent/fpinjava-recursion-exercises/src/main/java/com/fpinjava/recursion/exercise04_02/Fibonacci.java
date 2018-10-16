e0fe05cdcff97836f1ae7d084d2f9cbb08a4c45c
Renan Reis Martins de Paula <renan.reis@klarna.com>
2018-10-16 18:39:12 +0200

    chap 4 exercises - part 1

--- fpinjava-parent/fpinjava-recursion-exercises/src/main/java/com/fpinjava/recursion/exercise04_02/Fibonacci.java
+++ fpinjava-parent/fpinjava-recursion-exercises/src/main/java/com/fpinjava/recursion/exercise04_02/Fibonacci.java
@@ -1,11 +1,28 @@
 package com.fpinjava.recursion.exercise04_02;
 
+
+import com.fpinjava.common.TailCall;
+
 import java.math.BigInteger;
+
+import static com.fpinjava.common.TailCall.ret;
+import static com.fpinjava.common.TailCall.sus;
 
 
 public class Fibonacci {
 
   public static BigInteger fib(int x) {
-    throw new RuntimeException("To be implemented.");
+    return fib_(BigInteger.ONE, BigInteger.ZERO, BigInteger.valueOf(x)).eval();
+  }
+
+  private static TailCall<BigInteger> fib_(BigInteger acc1, BigInteger acc2,
+                                           BigInteger x) {
+    if (x.equals(BigInteger.ZERO)) {
+      return ret(BigInteger.ZERO);
+    } else if (x.equals(BigInteger.ONE)) {
+      return ret(acc1.add(acc2));
+    } else {
+      return sus(() -> fib_(acc2, acc1.add(acc2), x.subtract(BigInteger.ONE)));
+    }
   }
 }
--- fpinjava-parent/fpinjava-recursion-exercises/src/main/java/com/fpinjava/recursion/exercise04_03/FoldLeft.java
+++ fpinjava-parent/fpinjava-recursion-exercises/src/main/java/com/fpinjava/recursion/exercise04_03/FoldLeft.java
@@ -3,11 +3,24 @@ package com.fpinjava.recursion.exercise04_03;
 import java.util.List;
 
 import com.fpinjava.common.Function;
+import com.fpinjava.common.TailCall;
+
+import static com.fpinjava.common.CollectionUtilities.head;
+import static com.fpinjava.common.CollectionUtilities.tail;
 
 
 public class FoldLeft {
 
-  public static <T, U> U foldLeft(List<T> ts, U identity, Function<U, Function<T, U>> f) {
-    throw new RuntimeException("To be implemented.");
+  public static <T, U> U foldLeft(List<T> ts, U identity,
+                                  Function<U, Function<T, U>> f) {
+    return foldLeft_(ts, identity, f).eval(); //copied this line from the answer
+  }
+
+  private static <T, U> TailCall<U> foldLeft_(List<T> ts, U identity,
+                                       Function<U, Function<T, U>> f) {
+
+    return ts.isEmpty()
+            ? TailCall.ret(identity)
+            : TailCall.sus(() -> foldLeft_(tail(ts), f.apply(identity).apply(head(ts)), f));
   }
 }
