ba4c7990782e5a27bfedcbaace1ee9f0cbbae8d8
Renan Reis Martins de Paula <renan.reis@klarna.com>
2018-10-17 11:18:01 +0200

    range with tail rec and stack safe

--- fpinjava-parent/fpinjava-recursion-exercises/src/main/java/com/fpinjava/recursion/exercise04_04/Range.java
+++ fpinjava-parent/fpinjava-recursion-exercises/src/main/java/com/fpinjava/recursion/exercise04_04/Range.java
@@ -1,11 +1,23 @@
 package com.fpinjava.recursion.exercise04_04;
 
+import com.fpinjava.common.CollectionUtilities;
+import com.fpinjava.common.TailCall;
+
 import java.util.List;
+
+import static com.fpinjava.common.TailCall.ret;
+import static com.fpinjava.common.TailCall.sus;
 
 
 public class Range {
 
   public static List<Integer> range(Integer start, Integer end) {
-    throw new RuntimeException("To be implemented.");
+    return range_(start, end, CollectionUtilities.list()).eval();
+  }
+
+  public static TailCall<List<Integer>> range_(Integer start, Integer end, List<Integer> acc) {
+    return end <= start
+            ? ret(acc)
+            : sus(() -> range_(start + 1, end, CollectionUtilities.append(acc, start)));
   }
 }
