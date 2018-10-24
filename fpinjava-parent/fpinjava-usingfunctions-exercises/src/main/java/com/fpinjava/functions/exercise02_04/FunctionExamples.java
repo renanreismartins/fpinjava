1326301b48a9e09fdbb935f767c637e16d94f07b
Renan Reis Martins de Paula <renan.reis@klarna.com>
2018-10-24 19:53:00 +0200

    Exe 2.4. Here is where my current knowledge froze. I need to generify this to receive as many functions as needed.

--- fpinjava-parent/fpinjava-usingfunctions-exercises/src/main/java/com/fpinjava/functions/exercise02_04/FunctionExamples.java
+++ fpinjava-parent/fpinjava-usingfunctions-exercises/src/main/java/com/fpinjava/functions/exercise02_04/FunctionExamples.java
@@ -6,8 +6,29 @@ public class FunctionExamples {
 
   public static final Function<Integer, Integer> square = x -> x * x;
 
-  public static final Function<Function<Integer, Integer>, Function<Function<Integer, Integer>,
-                                        Function<Integer, Integer>>> compose = null; // To be implemented
+  //Write a function to compose the two functions square and triple used in exercise 2.2.
+  static Function<Function<Integer, Integer>, Function<Function<Integer, Integer>, Function<Integer, Integer>>> compose = new Function<Function<Integer, Integer>, Function<Function<Integer, Integer>, Function<Integer, Integer>>>() {
+    @Override
+    public Function<Function<Integer, Integer>, Function<Integer, Integer>> apply(Function<Integer, Integer> f1) {
+      return new Function<Function<Integer, Integer>, Function<Integer, Integer>>() {
+        @Override
+        public Function<Integer, Integer> apply(Function<Integer, Integer> f2) {
+          return new Function<Integer, Integer>() {
+            @Override
+            public Integer apply(Integer arg) {
+              return f1.apply(f2.apply(arg));
+            }
+          };
+        }
+      };
+    }
+  };
+
+  public void testInterface() {
+    compose.apply(triple).apply(square).apply(3);
+  }
+
+  Function<Function<Integer, Integer>, Function<Function<Integer, Integer>, Function<Integer, Integer>>> composeLambdaVersion = f1 -> f2 -> arg -> f1.apply(f2.apply(arg));
 
   public static final Function<Integer, Integer> f = compose.apply(square).apply(triple);
 }
