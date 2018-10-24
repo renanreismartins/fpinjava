5d6cf0de56c9f7e163b07c0dd101a4afcff795bb
Renan Reis Martins de Paula <renan.reis@klarna.com>
2018-10-24 19:45:41 +0200

    final of chap 4. Have to do some refresh on composition. Checking the exercises of chap 2 I realized the answers are not there, I did in another folder and did not commit. So I'm gonna review one by one before committing here

--- fpinjava-parent/fpinjava-recursion-exercises/src/main/java/com/fpinjava/recursion/exercise04_06/ComposeAll.java
+++ fpinjava-parent/fpinjava-recursion-exercises/src/main/java/com/fpinjava/recursion/exercise04_06/ComposeAll.java
@@ -8,6 +8,40 @@ import com.fpinjava.common.Function;
 public class ComposeAll {
 
   static <T> Function<T, T> composeAll(List<Function<T, T>> list) {
-    throw new RuntimeException("To be implemented.");
+
+    Function<Integer, Integer> f1 = i -> i + 1;
+    Function<Integer, Integer> f2 = i -> i + 2;
+    Function<Integer, Integer> f3 = i -> i + 3;
+
+
+    // A -> fA -> A
+
+
+    return null;
+  }
+
+  static <A> Function<A, A> compose(Function<A, A> f1, Function<A, A> f2) {
+    //Function<T, Function<T, F>> composed = T -> T -> F;
+
+    Function<A, A> composed = arg -> f1.apply(f2.apply(arg));
+    return composed;
+  }
+
+//  static <F, A> Function<Function<A, A>, Function<>> partial(Function<A, A> f1) {
+//    return null;
+//  }
+
+    static Function<Function<Integer, Integer>, Function<Function<Integer, Integer>, Function<Integer, Integer>> > composer = a -> b -> arg -> a.apply(b.apply(arg));
+
+  public static void main(String[] args) {
+    Function<Integer, Integer> f1 = i -> i + 1;
+    Function<Integer, Integer> f2 = i -> i + 2;
+    Function<Integer, Integer> f3 = i -> i + 3;
+
+
+
+    //System.out.println(f1.apply(f2.apply(f3.apply(0))));
+    System.out.println(f1.apply(f2.apply(0)));
+    System.out.println(composer.apply(f1).apply(f2).apply(0));
   }
 }
