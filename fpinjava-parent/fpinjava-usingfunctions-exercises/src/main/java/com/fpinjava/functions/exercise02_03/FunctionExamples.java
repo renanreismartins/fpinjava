3a529b75174e07efaa9e98ced801e28c99cc9228
Renan Reis Martins de Paula <renan.reis@klarna.com>
2018-10-24 19:51:08 +0200

    Exe 2.3

--- fpinjava-parent/fpinjava-usingfunctions-exercises/src/main/java/com/fpinjava/functions/exercise02_03/FunctionExamples.java
+++ fpinjava-parent/fpinjava-usingfunctions-exercises/src/main/java/com/fpinjava/functions/exercise02_03/FunctionExamples.java
@@ -3,7 +3,26 @@ package com.fpinjava.functions.exercise02_03;
 
 public class FunctionExamples {
 
-  public static final Function<Integer, Function<Integer, Integer>> add = null; // To be implemented
+  //Write a function to add two Integers.
+  // no lambdas.
+  static Function<Integer, Function<Integer, Integer>> add = new Function<Integer, Function<Integer, Integer>>() {
+    @Override
+    public Function<Integer, Integer> apply(Integer arg1) {
+      return new Function<Integer, Integer>() {
+        @Override
+        public Integer apply(Integer arg2) {
+          return arg1 + arg2;
+        }
+      };
+    }
+  };
+
+  public void testInterface() {
+    add.apply(2).apply(3);
+  }
+
+
+  Function<Integer, Function<Integer, Integer>> addLambdaVersion = arg1 -> arg2 -> arg1 + arg2;
 
   public static final BinaryOperator add2 =  null; // To be implemented
 
