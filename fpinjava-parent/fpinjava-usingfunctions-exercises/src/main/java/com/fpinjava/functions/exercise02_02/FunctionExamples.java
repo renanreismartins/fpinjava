0c615b582c0fb5b6242775f0e4e337c4b884797a
Renan Reis Martins de Paula <renan.reis@klarna.com>
2018-10-24 19:48:29 +0200

    Exe 0202, almost the same as previous

--- fpinjava-parent/fpinjava-usingfunctions-exercises/src/main/java/com/fpinjava/functions/exercise02_02/FunctionExamples.java
+++ fpinjava-parent/fpinjava-usingfunctions-exercises/src/main/java/com/fpinjava/functions/exercise02_02/FunctionExamples.java
@@ -1,13 +1,20 @@
 package com.fpinjava.functions.exercise02_02;
 
+import java.util.function.BiFunction;
+
 public class FunctionExamples {
 
   public static final Function<Integer, Integer> triple = x -> x * 3;
 
   public static final Function<Integer, Integer> square = x -> x * x;
 
-  public static final Function<Integer, Integer> compose(final Function<Integer, Integer> f1,
-                                                         final Function<Integer, Integer> f2) {
-    throw new RuntimeException("To be implemented.");
+  //Write a new version of the compose method by using lambdas.
+  public static Function<Integer, Integer> compose(Function<Integer, Integer> f1, Function<Integer, Integer> f2) {
+    return arg -> f1.apply(f2.apply(arg));
   }
+
+
+  // Extra: instead of method, use a function
+  BiFunction<Function<Integer, Integer>, Function<Integer, Integer>, Function<Integer, Integer>> compose =
+          (f1, f2) -> arg -> f1.apply(f2.apply(arg));
 }
