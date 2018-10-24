aa4f09749b7a988a1747a8553e7aaa02a48e568d
Renan Reis Martins de Paula <renan.reis@klarna.com>
2018-10-25 00:45:01 +0200

    Fix exercise

--- fpinjava-parent/fpinjava-usingfunctions-exercises/src/main/java/com/fpinjava/functions/exercise02_05/Function.java
+++ fpinjava-parent/fpinjava-usingfunctions-exercises/src/main/java/com/fpinjava/functions/exercise02_05/Function.java
@@ -6,7 +6,7 @@ public interface Function<T, U> {
 
   //Write a polymorphic version of the compose function.
   // didnt did this, but understood the concept, the previous implementation is not enough because how java treat the types
-  static <T, U, V> java.util.function.Function<java.util.function.Function<U, V>,java.util.function.Function<java.util.function.Function<T, U>, java.util.function.Function<T, V>>> higherCompose() {
+  static <T, U, V> Function<Function<U, V>,Function<Function<T, U>, Function<T, V>>> higherCompose() {
     return f -> g -> x -> f.apply(g.apply(x));
   }
 }
