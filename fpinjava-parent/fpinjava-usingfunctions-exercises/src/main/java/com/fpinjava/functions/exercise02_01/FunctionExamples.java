7cbe31e66cd16025a0cf75d6594fc96a89d7a2af
Renan Reis Martins de Paula <renan.reis@klarna.com>
2018-10-24 19:47:07 +0200

    Exe 02_01, nothing special so far

--- fpinjava-parent/fpinjava-usingfunctions-exercises/src/main/java/com/fpinjava/functions/exercise02_01/FunctionExamples.java
+++ fpinjava-parent/fpinjava-usingfunctions-exercises/src/main/java/com/fpinjava/functions/exercise02_01/FunctionExamples.java
@@ -1,5 +1,7 @@
 package com.fpinjava.functions.exercise02_01;
 
+import java.util.function.BiFunction;
+
 public class FunctionExamples {
 
   public static final Function<Integer, Integer> triple = new Function<Integer, Integer>() {
@@ -18,8 +20,27 @@ public class FunctionExamples {
     }
   };
 
-  public static final Function<Integer, Integer> compose(final Function<Integer, Integer> f1,
-                                                         final Function<Integer, Integer> f2) {
-    throw new RuntimeException("To be implemented.");
+  public static Function<Integer, Integer> compose(Function<Integer, Integer> f1, Function<Integer, Integer> f2) {
+    return new Function<Integer, Integer>() {
+      @Override
+      public Integer apply(Integer arg) {
+        return f1.apply(f2.apply(arg));
+      }
+    };
   }
-}
+
+  // Extra: instead of method, use a function
+  // No lambdas to make it didactic
+  BiFunction<Function<Integer, Integer>, Function<Integer, Integer>, Function<Integer, Integer>> compose =
+    new BiFunction<Function<Integer, Integer>, Function<Integer, Integer>, Function<Integer, Integer>>() {
+      @Override
+      public Function<Integer, Integer> apply(Function<Integer, Integer> f1, Function<Integer, Integer> f2) {
+        return new Function<Integer, Integer>() {
+          @Override
+          public Integer apply(Integer arg) {
+            return f1.apply(f2.apply(arg));
+          }
+        };
+      }
+    };
+  }
