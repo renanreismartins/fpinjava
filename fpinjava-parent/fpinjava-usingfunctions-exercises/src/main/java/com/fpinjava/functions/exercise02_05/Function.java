b848a52abf2e57f14c6e556b7d2545a119795af2
Renan Reis Martins de Paula <renan.reis@klarna.com>
2018-10-24 20:07:13 +0200

    Exe 2.5 as mentioned I did not do came with the fully solution, if I check Exe 4.06 I will noticed I try to use simpler types (only receive Integer and return it) Function<Function<Integer, Integer>, Function<Function<Integer, Integer>, Function<Integer, Integer>> > composer = a -> b -> arg -> a.apply(b.apply(arg)); then generalize it.... I failed generalizing it several times.

--- fpinjava-parent/fpinjava-usingfunctions-exercises/src/main/java/com/fpinjava/functions/exercise02_05/Function.java
+++ fpinjava-parent/fpinjava-usingfunctions-exercises/src/main/java/com/fpinjava/functions/exercise02_05/Function.java
@@ -4,7 +4,9 @@ public interface Function<T, U> {
 
   U apply(T arg);
 
-  static <T, U, V> Function<Function<U, V>, Function<Function<T, U>, Function<T, V>>> higherCompose() {
-    throw new RuntimeException("To be implemented.");
+  //Write a polymorphic version of the compose function.
+  // didnt did this, but understood the concept, the previous implementation is not enough because how java treat the types
+  static <T, U, V> java.util.function.Function<java.util.function.Function<U, V>,java.util.function.Function<java.util.function.Function<T, U>, java.util.function.Function<T, V>>> higherCompose() {
+    return f -> g -> x -> f.apply(g.apply(x));
   }
 }
