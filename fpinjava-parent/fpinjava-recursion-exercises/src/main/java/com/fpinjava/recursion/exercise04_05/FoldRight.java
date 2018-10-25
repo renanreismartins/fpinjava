4564793bd7528f9cd9ca40b1d9d7b8492d292851
Renan Reis Martins de Paula <renan.reis@klarna.com>
2018-10-25 08:43:20 +0200

    FoldRight tail recursive. In those cases, reverse the list, make the operation on the parameter passed to the tail rec call and use an accumulator

--- fpinjava-parent/fpinjava-recursion-exercises/src/main/java/com/fpinjava/recursion/exercise04_05/FoldRight.java
+++ fpinjava-parent/fpinjava-recursion-exercises/src/main/java/com/fpinjava/recursion/exercise04_05/FoldRight.java
@@ -3,10 +3,23 @@ package com.fpinjava.recursion.exercise04_05;
 import java.util.List;
 
 import com.fpinjava.common.Function;
+import com.fpinjava.common.TailCall;
+
+import static com.fpinjava.common.CollectionUtilities.head;
+import static com.fpinjava.common.CollectionUtilities.reverse;
+import static com.fpinjava.common.CollectionUtilities.tail;
 
 public class FoldRight {
 
   public static <T, U> U foldRight(List<T> ts, U identity, Function<T, Function<U, U>> f) {
-    throw new RuntimeException("To be implemented.");
+    return foldRight_(reverse(ts), identity, f).eval();
+  }
+
+  // had to use some help from the solution of the book for this :|
+
+  public static <T, U> TailCall<U> foldRight_(List<T> ts, U acc, Function<T, Function<U, U>> f) {
+    return ts.isEmpty()
+            ? TailCall.ret(acc)
+            : TailCall.sus(() -> foldRight_(tail(ts), f.apply(head(ts)).apply(acc), f));
   }
 }
