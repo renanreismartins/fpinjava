d3ffa2726778048c512e2b807b6517bff88aca18
Renan Reis Martins de Paula <renan.reis@klarna.com>
2018-10-28 23:24:45 +0100

    Exe 5.8 prod of elements of a list. tail rec, not stack safe

--- fpinjava-parent/fpinjava-lists-exercises/src/main/java/com/fpinjava/lists/exercise05_08/Product.java
+++ fpinjava-parent/fpinjava-lists-exercises/src/main/java/com/fpinjava/lists/exercise05_08/Product.java
@@ -5,6 +5,14 @@ import com.fpinjava.lists.exercise05_06.List;
 public class Product {
 
   public static Double product(List<Double> ints) {
-    throw new RuntimeException("To be implemented");
+    return product_(ints, 1D);
+  }
+
+  private static Double product_(List<Double> ints, Double acc) {
+
+    return ints.isEmpty()
+            ? acc
+            : product_(ints.tail(), acc * ints.head());
+
   }
 }
