8e5e1f32d3fc548390e331ff32fd7c989897f709
Renan Reis Martins de Paula <renan.reis@klarna.com>
2018-10-28 23:21:49 +0100

    Exe 5.7 sum elements of a list. tail rec, not stack safe

--- fpinjava-parent/fpinjava-lists-exercises/src/main/java/com/fpinjava/lists/exercise05_07/Sum.java
+++ fpinjava-parent/fpinjava-lists-exercises/src/main/java/com/fpinjava/lists/exercise05_07/Sum.java
@@ -5,6 +5,14 @@ import com.fpinjava.lists.exercise05_06.List;
 public class Sum {
 
   public static Integer sum(List<Integer> ints) {
-    throw new RuntimeException("To be implemented");
+    return sum_(ints, 0);
+  }
+
+  private static Integer sum_(List<Integer> ints, Integer acc) {
+
+    return ints.isEmpty()
+            ? acc
+            : sum_(ints.tail(), acc + ints.head());
+
   }
 }
