2d445fe9b32486df8717635309d75cf5aa8088ba
Renan Reis Martins de Paula <renan.reis@klarna.com>
2018-10-29 23:31:39 +0100

    exe 5.12 reverse in terms of foldLeft

--- fpinjava-parent/fpinjava-lists-exercises/src/main/java/com/fpinjava/lists/exercise05_12/Reverse.java
+++ fpinjava-parent/fpinjava-lists-exercises/src/main/java/com/fpinjava/lists/exercise05_12/Reverse.java
@@ -2,9 +2,10 @@ package com.fpinjava.lists.exercise05_12;
 
 import com.fpinjava.lists.exercise05_10.List;
 
+
 public class Reverse {
 
   public static <A> List<A> reverseViaFoldLeft(List<A> list) {
-    throw new IllegalStateException("To be implemented");
+    return list.foldLeft(List.list(), l -> a -> l.cons(a));
   }
 }
