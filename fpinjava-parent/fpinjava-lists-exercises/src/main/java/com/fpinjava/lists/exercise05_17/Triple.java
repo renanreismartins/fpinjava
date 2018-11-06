2644b7cedcd2c098fe36a196df3c085e6d6b1023
Renan Reis Martins de Paula <renan.reis@klarna.com>
2018-11-06 13:53:40 +0100

    Did not work because i was using the wrong import

--- fpinjava-parent/fpinjava-lists-exercises/src/main/java/com/fpinjava/lists/exercise05_17/Triple.java
+++ fpinjava-parent/fpinjava-lists-exercises/src/main/java/com/fpinjava/lists/exercise05_17/Triple.java
@@ -2,10 +2,12 @@ package com.fpinjava.lists.exercise05_17;
 
 import com.fpinjava.lists.exercise05_16.List;
 
+import static com.fpinjava.common.List.cons;
+
 public class Triple {
 
   public static List<Integer> triple(List<Integer> list) {
-    //return List.foldRight(list, List.<Integer>list(), x -> y -> cons(x * 3, y)); WHY DOES NOT IT WORK?
+    //return List.foldRight(list, List.<Integer>list(), x -> y -> cons(x * 3, y)); //WHY DOES NOT IT WORK?
 
     return List.foldRight(list, List.<Integer>list(), a -> b ->
             b.cons(a * 3));
