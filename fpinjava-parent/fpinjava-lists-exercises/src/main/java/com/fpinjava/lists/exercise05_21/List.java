c32cf301be0ac8fa89b4eecd84499d467074cc2a
Renan Reis Martins de Paula <renan.reis@klarna.com>
2018-11-06 13:56:32 +0100

    5.21 flatmap in terms of foldRight

--- fpinjava-parent/fpinjava-lists-exercises/src/main/java/com/fpinjava/lists/exercise05_21/List.java
+++ fpinjava-parent/fpinjava-lists-exercises/src/main/java/com/fpinjava/lists/exercise05_21/List.java
@@ -20,7 +20,7 @@ public abstract class List<A> {
   public abstract <B> B foldLeft(B identity, Function<B, Function<A, B>> f);
 
   public <B> List<B> flatMap(Function<A, List<B>> f) {
-    throw new IllegalStateException("To be implemented");
+    return foldRight(list(), h -> t -> concat(f.apply(h), t));
   }
 
   public List<A> filter(Function<A, Boolean> f) {
