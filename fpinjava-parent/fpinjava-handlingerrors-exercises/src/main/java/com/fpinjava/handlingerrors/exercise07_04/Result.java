096236d5495a56928d4fbe93308328174d71d5ce
Renan Reis Martins de Paula <renan.reis@klarna.com>
2018-11-13 08:41:29 +0100

    flatMap, getOrElse, and orElse for the Result class

--- fpinjava-parent/fpinjava-handlingerrors-exercises/src/main/java/com/fpinjava/handlingerrors/exercise07_04/Result.java
+++ fpinjava-parent/fpinjava-handlingerrors-exercises/src/main/java/com/fpinjava/handlingerrors/exercise07_04/Result.java
@@ -20,7 +20,7 @@ public abstract class Result<V> implements Serializable {
   public abstract <U> Result<U> flatMap(Function<V, Result<U>> f);
 
   public Result<V> orElse(Supplier<Result<V>> defaultValue) {
-    throw new RuntimeException("To be implemented");
+    return map(x -> this).getOrElse(defaultValue);
   }
 
   private static class Failure<V> extends Result<V> {
@@ -49,22 +49,22 @@ public abstract class Result<V> implements Serializable {
 
     @Override
     public V getOrElse(V defaultValue) {
-      throw new RuntimeException("To be implemented");
+      return defaultValue;
     }
 
     @Override
     public V getOrElse(Supplier<V> defaultValue) {
-      throw new RuntimeException("To be implemented");
+      return defaultValue.get();
     }
 
     @Override
     public <U> Result<U> map(Function<V, U> f) {
-      throw new RuntimeException("To be implemented");
+      return failure(exception);
     }
 
     @Override
     public <U> Result<U> flatMap(Function<V, Result<U>> f) {
-      throw new RuntimeException("To be implemented");
+      return failure(exception);
     }
   }
 
@@ -84,22 +84,22 @@ public abstract class Result<V> implements Serializable {
 
     @Override
     public V getOrElse(V defaultValue) {
-      throw new RuntimeException("To be implemented");
+      return value;
     }
 
     @Override
     public V getOrElse(Supplier<V> defaultValue) {
-      throw new RuntimeException("To be implemented");
+      return value;
     }
 
     @Override
     public <U> Result<U> map(Function<V, U> f) {
-      throw new RuntimeException("To be implemented");
+      return success(f.apply(value));
     }
 
     @Override
     public <U> Result<U> flatMap(Function<V, Result<U>> f) {
-      throw new RuntimeException("To be implemented");
+      return f.apply(value);
     }
   }
 
