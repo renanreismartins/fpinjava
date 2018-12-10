d4035fd749e219e42c765bb79a1fdeaec58f834a
Renan Reis Martins de Paula <renan.reis@klarna.com>
2018-12-10 09:25:52 +0100

    flatMap in Streams

--- fpinjava-parent/fpinjava-laziness-exercises/src/main/java/com/fpinjava/laziness/exercise09_13/Stream.java
+++ fpinjava-parent/fpinjava-laziness-exercises/src/main/java/com/fpinjava/laziness/exercise09_13/Stream.java
@@ -31,7 +31,7 @@ abstract class Stream<A> {
   public abstract <B> B foldRight(Supplier<B> z, Function<A, Function<Supplier<B>, B>> f);
 
   public <B> Stream<B> flatMap(Function<A, Stream<B>> f) {
-    throw new IllegalStateException("To be implemented");
+    return foldRight(Stream::empty, a -> acc -> f.apply(a).append(acc));
   }
 
   public Stream<A> append(Supplier<Stream<A>> s) {
