493dd5806839b86c5b25a985500395251d777c32
Renan Reis Martins de Paula <renan.reis@klarna.com>
2018-12-20 13:12:23 +0100

    foldPreOrder and foldPostOrder

--- fpinjava-parent/fpinjava-trees-exercises/src/main/java/com/fpinjava/trees/exercise10_09/Tree.java
+++ fpinjava-parent/fpinjava-trees-exercises/src/main/java/com/fpinjava/trees/exercise10_09/Tree.java
@@ -269,12 +269,12 @@ public abstract class Tree<A extends Comparable<A>> {
 
     @Override
     public <B> B foldPreOrder(B identity, Function<A, Function<B, Function<B, B>>> f) {
-      throw new IllegalStateException("To be implemented");
+      return f.apply(value).apply(left.foldPreOrder(identity, f)).apply(right.foldPreOrder(identity, f));
     }
 
     @Override
     public <B> B foldPostOrder(B identity, Function<B, Function<B, Function<A, B>>> f) {
-      throw new IllegalStateException("To be implemented");
+      return f.apply(left.foldPostOrder(identity, f)).apply(right.foldPostOrder(identity, f)).apply(value);
     }
 
     @Override
