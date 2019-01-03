59d4eb57bb80944e9c9eba06b46a7a4ad40b2420
Renan Reis Martins de Paula <renanreismartins@gmail.com>
2019-01-03 17:27:50 +0100

    Read from the console and print. Using the same input to read from console

--- fpinjava-parent/fpinjava-io-exercises/src/main/java/com/fpinjava/io/exercise13_03/ReadConsole.java
+++ fpinjava-parent/fpinjava-io-exercises/src/main/java/com/fpinjava/io/exercise13_03/ReadConsole.java
@@ -1,9 +1,25 @@
 package com.fpinjava.io.exercise13_03;
 
 
+import com.fpinjava.common.Result;
+import com.fpinjava.common.Stream;
+import com.fpinjava.common.Tuple;
+
 public class ReadConsole {
 
   public static void main(String... args) {
-    throw new IllegalStateException("To be implemented");
+    Stream<Person> stream = Stream.unfold(ConsoleReader.consoleReader(), ReadConsole::person);
+    stream.toList().forEach(System.out::println);
+  }
+
+  public static Result<Tuple<Person, Input>> person(Input input) {
+    Result<Tuple<Integer, Input>> rId = input.readInt("Id:");
+    Result<Tuple<String, Input>> rFirstName = input.readString("First Name:");
+    Result<Tuple<String, Input>> rLastName = input.readString("Last Name:");
+
+    return rId.flatMap(tId -> rFirstName.flatMap(tFirstName ->
+                                                         rLastName.flatMap(tLastName ->
+                                                                                   Result.success(new Tuple<>(Person.apply(tId._1, tFirstName._1, tLastName._1), tLastName._2)))));
+
   }
 }
